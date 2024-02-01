05ee2c29a319cd6fac58b22eea8f5e6150bd966a
Nikhita-Palla <pallanikhita@gmail.com>
2024-02-02 00:57:20 +0530

    Added 01-02-2024 readings

---
+++ 01-02-2024/Codes_Practice/IEnnumerable_IEnnumerator/Program.cs
@@ -0,0 +1,28 @@
+﻿class Program
+{
+  
+    public static void Main(String[] args)
+    {
+        List<int> list = new List<int>();
+        list.Add(1);
+        list.Add(2);
+        list.Add(3);
+        list.Add(4);
+        list.Add(5);
+
+        IEnumerable<int> ienumerable=(IEnumerable<int>)list;
+
+        foreach(int i in ienumerable)
+        {
+            Console.WriteLine(i);
+        }
+        
+        IEnumerator<int> ienumerator=list.GetEnumerator();
+        while(ienumerator.MoveNext())
+        {
+            Console.WriteLine(ienumerator.Current);
+        }
+        
+
+    }
+}
---
+++ 01-02-2024/Codes_Practice/Implicit_casting/Program.cs
@@ -0,0 +1,17 @@
+﻿class Program
+{
+    public static void Main(String[] args)
+    {
+        byte a = 255;
+        int b = a;
+        Console.WriteLine(b+" Type after converting byte to int "+b.GetType());
+        long l = b;
+        Console.WriteLine(l + " Type after converting int to long " + l.GetType());
+        float f = b;
+        Console.WriteLine(f + " Type after converting int to float " + f.GetType());
+        decimal d = l;
+        Console.WriteLine(d + " Type after converting long to decimal " + d.GetType());
+        double db = f;
+        Console.WriteLine(db + " Type after converting float to double " + db.GetType());
+    }
+}
